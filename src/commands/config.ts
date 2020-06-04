2e56026b811418cef926c4396f90959debc13a25
Yanhui Shen <shen.elf@gmail.com>
2020-06-04 14:01:55 +0800

    Cleanup

--- src/commands/config.ts
+++ src/commands/config.ts
@@ -13,11 +13,5 @@ export class ConfigDumpCommand implements Command {
   async run() {
     const text = JSON.stringify(context.config, undefined, 2);
     context.logger.info(`The merged config is:\n${text}`);
-    // const response = await context.axios.get('https://bing.com');
-    // context.logger.info(response.data);
-    // const results = await context.transactional(async (transaction) => {
-    //   return await context.sequelize.query('select * from data limit 10', { transaction });
-    // });
-    // context.logger.info({ results });
   }
 }
